6d1213e1d482c77c9135917243a6a01b7007ed1e
Artem <neznajkinartem357@gmail.com>
2022-11-20 16:19:28 +0600

    first migration medicines

--- medicines/models.py
+++ medicines/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from libs.base_models import NamedModel
+
+
+class Medicine(NamedModel, TimeStampedModel):
+    price = models.FloatField(blank=True, verbose_name=("Цена"), null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Препарат")
+        verbose_name_plural = _("Препараты")
+        ordering = ["-created"]
+
