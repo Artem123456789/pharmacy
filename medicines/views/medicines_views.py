042637234880d26c7ad4bdedc86be3e88b322f1d
Artem <neznajkinartem357@gmail.com>
2022-11-27 15:38:47 +0600

    pagination added

--- medicines/views/medicines_views.py
+++ medicines/views/medicines_views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+
+from libs.pagination import StandardResultsSetPagination
 from medicines.serializers import medicines_serializers
 from medicines.models import Medicine
 
@@ -8,3 +10,4 @@ class MedicinesList(generics.ListAPIView):
     serializer_class = medicines_serializers.MedicineSerializer
     queryset = Medicine.objects.all()
     permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
