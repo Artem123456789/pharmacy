a9acaeeb7f8d68ed9ba32b81dce51fa8b4125a10
artem <zhiruyev_a@bankrkb.kz>
2022-12-29 15:40:25 +0600

    max page size changed

--- medicines/paginators.py
+++ medicines/paginators.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class MedicinesListPaginator(PageNumberPagination):
     page_size_query_param = "page_size"
-    page_size = 5
+    page_size = 9
     max_page_size = 100
